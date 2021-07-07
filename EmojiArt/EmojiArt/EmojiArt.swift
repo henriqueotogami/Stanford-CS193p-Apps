580d2ccea1a9517134c8662c5157c2fe9651ca4c
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-07-06 22:31:39 -0300

    Aula 7: Multithreading EmojiArt
    Nesta parte da aula, o app EmojiArt recebe uma imagem da web, através da multitarefa do iPadOS, e sobre ela é possível adicionar alguns emojis pré selecionados.

---
+++ EmojiArt/EmojiArt/EmojiArt.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Henrique Matheus Alves Pereira on 06/07/21.
+//
+
+import Foundation
+
+struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int // Offset from center
+        var y: Int // Offset from center
+        var size: Int
+        var id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+}
--- EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -17,13 +17,54 @@ struct EmojiArtDocumentView: View {
                     ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
                         Text(emoji)
                             .font(Font.system(size: self.defaultEmojiSize))
+                            .onDrag{ NSItemProvider(object: emoji as NSString)}
                     }
                 }
             }
             .padding(.horizontal)
-            Rectangle().foregroundColor(.yellow)
-                .edgesIgnoringSafeArea([.horizontal, .bottom])
+            GeometryReader { geometry in
+                ZStack {
+                Color.white.overlay(
+                    Group {
+                        if self.document.backgroundImage != nil {
+                            Image(uiImage: self.document.backgroundImage!)
+                        }
+                    }
+                )
+                    .edgesIgnoringSafeArea([.horizontal, .bottom])
+                    .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
+                        var location = geometry.convert(location, from: .global)
+                        location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                        return self.drop(providers: providers, at: location)
+                    }
+                ForEach(self.document.emojis) { emoji in
+                    Text(emoji.text)
+                        .font(self.font(for: emoji))
+                        .position(self.position(for: emoji, in: geometry.size))
+                    }
+                }
+            }
         }
+    }
+    private func font(for emoji: EmojiArt.Emoji) -> Font {
+        Font.system(size: emoji.fontSize)
+    }
+
+    private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
+        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+    }
+
+    private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
+        var found = providers.loadFirstObject(ofType: URL.self) { url in
+            print("dropped \(url)")
+            self.document.setBackgroundURL(url)
+        }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                self.document.addEmoji(string, at: location, size: self.defaultEmojiSize)
+            }
+        }
+        return found
     }
     private let defaultEmojiSize: CGFloat = 40
 }
