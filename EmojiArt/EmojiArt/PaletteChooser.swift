6744552d016bd985bcb0c2a774479c036e4fcdb4
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-07-13 22:53:07 -0300

    Aula 10: Adicionar e remover emoji
    EmojiArt para iPadOS, utilizando o arquivo Grid do app Memorize, com algumas modificações.

--- EmojiArt/EmojiArt/PaletteChooser.swift
+++ EmojiArt/EmojiArt/PaletteChooser.swift
@@ -28,8 +28,9 @@ struct PaletteChooser: View {
                 .onTapGesture {
                     self.showPaletteEditor = true
                 }
-                .popover(isPresented: $showPaletteEditor){
+                .sheet(isPresented: $showPaletteEditor){
                     PaletteEditor(chosenPalette: $chosenPalette)
+                        .environmentObject(self.document)
                         .frame(minWidth: 300, minHeight: 500)
                 }
         }
@@ -38,19 +39,55 @@ struct PaletteChooser: View {
 }
 
 struct PaletteEditor: View {
+    @EnvironmentObject var document: EmojiArtDocument
     @Binding var chosenPalette: String
+    @State private var paletteName: String = ""
+    @State private var emojisToAdd: String = ""
     
     var body: some View {
-        VStack {
-            Text("Palette Editor")
+        VStack(spacing: 0) {
+            Text("Palette Editor").font(.headline).padding()
             Divider()
-            Text(chosenPalette)
+            Form {
+                Section {
+                    TextField("Palette Name", text: $paletteName, onEditingChanged: { began in
+                        if !began {
+                            self.document.renamePalette(self.chosenPalette, to: self.paletteName)
+                        }
+                    })
+                    TextField("Add Emoji", text: $emojisToAdd, onEditingChanged: { began in
+                        if !began {
+                            self.chosenPalette = self.document.addEmoji(self.emojisToAdd, toPalette: self.chosenPalette)
+                            self.emojisToAdd = ""
+                        }
+                    })
+                }
+                Section (header: Text("Remove Emoji")) {
+                    Grid(chosenPalette.map { String($0) }, id: \.self) { emoji in
+                        Text(emoji).font(Font.system(size: self.fontSize))
+                            .onTapGesture {
+                                self.chosenPalette = self.document.removeEmoji(emoji, fromPalette: self.chosenPalette)
+                        }
+                    }
+                    .frame(height: self.height)
+                }
+            }
         }
+        .onAppear { self.paletteName = self.document.paletteNames[self.chosenPalette] ?? "" }
     }
+    
+    // MARK: -- Drawing Constants
+    
+    var height: CGFloat {
+        CGFloat((chosenPalette.count - 1) / 6) * 70 + 70
+    }
+    let fontSize: CGFloat = 40
 }
 
+/*
 struct PaletteChooser_Previews: PreviewProvider {
     static var previews: some View {
         PaletteChooser(document: EmojiArtDocument(), chosenPalette: Binding.constant(""))
     }
 }
+*/
