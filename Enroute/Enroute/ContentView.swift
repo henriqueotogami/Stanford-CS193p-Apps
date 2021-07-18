50f5872098f3ffe40f57c3266319e58766afff2e
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-07-18 20:38:17 -0300

    Aula 12: Utilizando Core Data no Enroute app

---
+++ Enroute/Enroute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Enroute
+//
+//  Created by Henrique Matheus Alves Pereira on 18/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
