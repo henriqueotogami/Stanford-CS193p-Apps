b20464cf4cdda42a31899fd2b07988f4f06ceaf4
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-07-19 22:19:36 -0300

    Aula 12: Implementando estrutura do banco de dados

--- Enroute/Enroute/SceneDelegate.swift
+++ Enroute/Enroute/SceneDelegate.swift
@@ -23,7 +23,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
+        let airport = Airport.withICAO("KSFO", context: context)
+        let contentView = FlightsEnrouteView(flightSearch: FlightSearch(destination: airport))
+            .environment(\.managedObjectContext, context)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
